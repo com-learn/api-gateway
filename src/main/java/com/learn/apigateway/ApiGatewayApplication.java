282f687d58f55c82f4de2c447550d3543d0118f8
com-learn <midoshkm@hotmail.com>
2021-07-19 23:50:32 +0530

    loadbalanced client

--- src/main/java/com/learn/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/learn/apigateway/ApiGatewayApplication.java
@@ -3,13 +3,11 @@ package com.learn.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class ApiGatewayApplication {
 
